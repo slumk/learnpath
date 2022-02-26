242470b8506f189b134c88fb36b5c959ac46a2d9
slumk <salahucknn@gmail.com>
2022-02-26 14:51:23 +0530

    Context came into action

--- learnpath/src/App.jsx
+++ learnpath/src/App.jsx
@@ -1,20 +1,31 @@
 import { NavBarComponent } from './resources/home_fixed/navBar.jsx'
 import { MainPageComponent } from './resources/home_fixed/mainPage.jsx'
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
 export const AuthContext = createContext()
 
 export const App = () => {
+  let cookieValue = document.cookie.split('; ')
+  if (cookieValue) {
+    cookieValue = cookieValue.find(row => row.startsWith('learnpath-key'))
+  }
+  if (cookieValue) {
+    cookieValue = cookieValue.split('=')[1]
+  }
   const initialAuthData = {
     isLoggedin: false,
     is_teacher: false,
     is_mod: false,
     is_god: false
   }
+  if (cookieValue === 'true') {
+    initialAuthData.isLoggedin = true
+  }
+  const [auth, setAuth] = useState(initialAuthData)
   document.title = 'Learnpath - Home'
   return (
-    <AuthContext.Provider value = {initialAuthData}>
+    <AuthContext.Provider value = {{ auth, setAuth }}>
   <div className='h-screen relative'>
-      <div className='m-auto px-1'>
+      <div>
           <div><NavBarComponent/></div>
       </div>
       <div>
