299b6d9cb26261845556090f3b23550cc9cdf5b6
slumk <salahucknn@gmail.com>
2022-02-28 23:15:09 +0530

    Refactored HTML, and Root app to make it readable

--- learnpath/src/App.jsx
+++ learnpath/src/App.jsx
@@ -1,28 +1,30 @@
-import { NavBarComponent } from './resources/home_fixed/navBar.jsx'
+import NavBarComponent from './resources/home_fixed/navBar.jsx'
 import { MainPageComponent } from './resources/home_fixed/mainPage.jsx'
 import { createContext, useState } from 'react'
+
 export const AuthContext = createContext()
+export const GodContext = createContext() // Admin is god here
+
+const initialAuthData = {
+  isLoggedin: false,
+  is_teacher: false,
+  is_mod: false
+}
+
+const initialGodData = {
+  isGodHere: true
+}
 
 export const App = () => {
-  let cookieValue = document.cookie.split('; ')
-  if (cookieValue) {
-    cookieValue = cookieValue.find(row => row.startsWith('learnpath-key'))
-  }
-  if (cookieValue) {
-    cookieValue = cookieValue.split('=')[1]
-  }
-  const initialAuthData = {
-    isLoggedin: false,
-    is_teacher: false,
-    is_mod: false,
-    is_god: false
-  }
+  const cookieValue = getCookie()
   if (cookieValue === 'true') {
     initialAuthData.isLoggedin = true
   }
   const [auth, setAuth] = useState(initialAuthData)
+  const [god, setGodPlace] = useState(initialGodData)
   document.title = 'Learnpath - Home'
   return (
+    <GodContext.Provider value={{ god, setGodPlace }}>
     <AuthContext.Provider value = {{ auth, setAuth }}>
   <div className='h-screen relative'>
       <div>
@@ -33,5 +35,18 @@ export const App = () => {
       </div>
       </div>
       </AuthContext.Provider>
+      </GodContext.Provider>
   )
+}
+
+const getCookie = () => {
+  let cookieValue = document.cookie.split('; ')
+  if (cookieValue) {
+    cookieValue = cookieValue.find(row => row.startsWith('learnpath-key'))
+  }
+  if (cookieValue) {
+    cookieValue = cookieValue.split('=')[1]
+    return cookieValue
+  }
+  return false
 }
