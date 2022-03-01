d6b1f533862665f13c69f746967fb7f5eaf5eea2
slumk <salahucknn@gmail.com>
2022-03-02 00:47:23 +0530

    Removed pre hook from admin model

--- api/resources/admin/admin.model.js
+++ api/resources/admin/admin.model.js
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose'
-import bcrypt from 'bcryptjs'
 
 const adminSchema = new mongoose.Schema(
 	{
@@ -17,13 +16,4 @@ const adminSchema = new mongoose.Schema(
 		},
 	}
 )
-
-adminSchema.pre('save', async (next) => {
-	this.password = await bcrypt.hash(this.password, 10)
-		.then((hash) => {
-			return hash
-		})
-	next()
-})
-
-export const adminModel = new mongoose.Model('Admin', adminSchema)
+export const adminModel = new mongoose.model('Admin', adminSchema)
