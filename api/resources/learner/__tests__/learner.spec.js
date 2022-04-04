c9aac6a527f92cd12ec53cf1af496d6a64ba313a
slumk <salahucknn@gmail.com>
2022-04-04 16:15:59 +0530

    Updated Tests

--- api/resources/learner/__tests__/learner.spec.js
+++ api/resources/learner/__tests__/learner.spec.js
@@ -47,21 +47,19 @@ beforeAll(async () => {
 })
 
 test('Upvoting Capsule', async () => {
-	await upvoteCapsule('620fa734dd24eb1316beabff')
+	await upvoteCapsule('620fa734dd24eb1316beacff','620fa734dd24eb1316beabff')
 	const capsule = await capsuleModel.findById('620fa734dd24eb1316beabff')
+	const user = await learnerModel.findById('620fa734dd24eb1316beacff')
 	expect(await capsule.upvote_count).toBe(1)
+	expect(await user.upvoted_capsules).not.toBe([])
 })
 
 test('Un-Upvotes a capsule', async () => {
-	await minusUpvoteCapsule('620fa734dd24eb1316beabff')
+	await minusUpvoteCapsule('620fa734dd24eb1316beacff', '620fa734dd24eb1316beabff')
 	const capsule = await capsuleModel.findById('620fa734dd24eb1316beabff')
 	expect(capsule.upvote_count).toBe(0)
-})
-
-test('Request account deletion', async () => {
-	await requestAccountDeletion('620fa734dd24eb1316beacff')
-	const user = await learnerModel.findById('620fa734dd24eb1316beacff').lean()
-	expect(user.requested_delete).toBeTruthy()
+	const user = await learnerModel.findById('620fa734dd24eb1316beacff')
+	expect(await user.upvoted_capsules).toStrictEqual([])
 })
 
 test('Bookmarks Capsule', async () => {
