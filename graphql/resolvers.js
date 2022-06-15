70ba6f6d9a8c7078b4273b8a8ff8dd954d0d08e2
slumk <salahucknn@gmail.com>
2022-06-15 22:29:39 +0530

    Fixed schema and resolvers

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,15 +1,10 @@
 import { capsuleResolver } from '../resources/capsule/capsule.resolvers.js'
-import { learnerResolver } from '../resources/learner/learner.resolver.js'
-import { learnerAuthResolver } from '../utils/auth/learner.auth.resolver.js'
+import { commentResolver } from '../resources/capsule/comment/comment.resolver.js'
 export const fullResolvers = {
 	Query: {
-		...capsuleResolver.Query,
-		...learnerResolver.Query,
-		...learnerAuthResolver.Query
+		...capsuleResolver.Query
 	},
 	Mutation: {
-		...capsuleResolver.Mutation,
-		...learnerResolver.Mutation,
-		...learnerAuthResolver.Mutation
+		...capsuleResolver.Mutation
 	}
 }
--- graphql/schema.js
+++ graphql/schema.js
@@ -1,13 +1,11 @@
 import { capsuleTypeDefs } from '../resources/capsule/capsule.types.js'
-import { learnerTypeDefs } from '../resources/learner/learner.types.js'
-import { learnerAuthTypeDefs } from '../utils/auth/learner.auth.types.js'
+import { commentTypeDefs } from '../resources/capsule/comment/comment.types.js'
 import { gql } from 'apollo-server-core'
 
 let typeDef = []
 const typesArray = [
-	learnerAuthTypeDefs,
 	capsuleTypeDefs,
-	learnerTypeDefs
+	commentTypeDefs
 ]
 typesArray.forEach(eachTypeDef => {
 	typeDef.push(eachTypeDef)
